29598b88159ce7bdb5680d7bb0a8808f4c2e8af9
Dell <somayamahmoud30@gmail.com>
2024-11-01 23:12:15 +0200

    test: add unit testing to forget password

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -47,6 +47,11 @@ import '../features/questions/data/data_sources/questions_oline_data_source.dart
     as _i819;
 import '../features/questions/data/data_sources/questions_oline_data_source_impl.dart'
     as _i290;
+import '../features/questions/data/repositories/questions_repo_impl.dart'
+    as _i852;
+import '../features/questions/domain/repositories/questions_repo.dart' as _i828;
+import '../features/questions/domain/use_cases/get_questions_for_exam_use_case.dart'
+    as _i696;
 import '../features/survey/data/data_sources/remote_data_source/survey_api_remote_data_source.dart'
     as _i252;
 import '../features/survey/data/data_sources/remote_data_source/survey_remote_data_source.dart'
@@ -77,10 +82,6 @@ extension GetItInjectableX on _i174.GetIt {
         () => registerModule.navigatorKey);
     gh.lazySingleton<_i361.Dio>(() => networkFactory.provideDio());
     gh.singleton<_i282.ApiManager>(() => _i282.ApiManager(gh<_i361.Dio>()));
-    gh.factory<_i819.QuestionsOlineDataSource>(
-        () => _i290.QuestionsOlineDataSourceImpl(gh<_i282.ApiManager>()));
-    gh.lazySingleton<_i609.SurveyRemoteDataSource>(
-        () => _i252.SurveyApiRemoteDataSource(gh<_i282.ApiManager>()));
     gh.factory<_i637.AuthOnlineDatasource>(
         () => _i757.AuthOnlineDatasourceImpl(gh<_i282.ApiManager>()));
     gh.lazySingleton<_i609.SurveyRemoteDataSource>(
@@ -91,6 +92,8 @@ extension GetItInjectableX on _i174.GetIt {
         () => _i900.SurveyRepositoryIml(gh<_i609.SurveyRemoteDataSource>()));
     gh.factory<_i869.AuthRepository>(
         () => _i990.AuthRepoImpl(gh<_i637.AuthOnlineDatasource>()));
+    gh.factory<_i819.QuestionsOlineDataSource>(
+        () => _i290.QuestionsOlineDataSourceImpl(gh<_i282.ApiManager>()));
     gh.factory<_i87.ExamRepo>(
         () => _i66.ExamRepoImpl(gh<_i1006.ExamOnlineDatasource>()));
     gh.factory<_i730.GetSubjectsUseCase>(
@@ -112,6 +115,8 @@ extension GetItInjectableX on _i174.GetIt {
           gh<_i642.VerifyResetCodeUseCase>(),
           gh<_i906.ResetPasswordUseCase>(),
         ));
+    gh.factory<_i828.QuestionsRepo>(
+        () => _i852.QuestionsRepoImpl(gh<_i819.QuestionsOlineDataSource>()));
     gh.factory<_i1070.LoginViewModel>(
         () => _i1070.LoginViewModel(gh<_i496.LoginUseCase>()));
     gh.factory<_i541.RegisterCubit>(
@@ -120,6 +125,8 @@ extension GetItInjectableX on _i174.GetIt {
         () => _i579.SubjectCubit(gh<_i730.GetSubjectsUseCase>()));
     gh.factory<_i1060.ExamCubit>(
         () => _i1060.ExamCubit(gh<_i984.GetAllExamsUseCase>()));
+    gh.factory<_i696.GetQuestionsForExamUseCase>(
+        () => _i696.GetQuestionsForExamUseCase(gh<_i828.QuestionsRepo>()));
     return this;
   }
 }
